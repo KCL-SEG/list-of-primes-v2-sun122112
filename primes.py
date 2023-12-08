0a008890c25a5f32e564d16a007caf9b9d21bdfc
Sun Lee <sun.lee@kcl.ac.uk>
2023-12-08 13:06:46 +0000

    Add solution

--- primes.py
+++ primes.py
@@ -1,6 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 ==0:
+        return False
+    for i in range(3, int(num/2)+1):
+        if (num % i) == 0:
+            return False
+    return True
+
+
 def primes(number_of_primes):
+    if number_of_primes <=0:
+        raise ValueError
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if isPrime(num):
+            list.append(num)
+        num += 1
+
     return list
